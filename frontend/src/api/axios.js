72bfd5f835da2797f6be350e2de8c84bf784c471
Adrian263-lab <adrianizquierdomejias@gmail.com>
2026-05-19 19:30:21 +0200

    mejoras

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost/api', // Tu URL de Caddy o Backend original
+    baseURL: 'http://82.223.122.129/api', 
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
