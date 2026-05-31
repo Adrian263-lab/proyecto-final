defaea40cc3eecf0fc0719da6a0c4e6a513d0170
Adrian263-lab <adrianizquierdomejias@gmail.com>
2026-05-31 19:51:09 +0200

    mejoras

--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -89,29 +89,47 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $this->call([
-            // He comentado tus otros seeders para evitar que generen conflictos con las cantidades solicitadas
-            // ProtectoraSeeder::class,
-            // EventoSeeder::class,
+            // Seeders secundarios desactivados para delegar todo el volumen al bucle maestro
         ]);
+
+        // Pool de fotos exclusivas para las 15 protectoras dinámicas (evita imágenes repetidas)
+        $fotosSecuenciales = [
+            1 => 'https://images.unsplash.com/photo-1516734212186-a967f81ad0d7',
+            2 => 'https://images.unsplash.com/photo-1583511655857-d19b40a7a54e',
+            3 => 'https://images.unsplash.com/photo-1543466835-00a7907e9de1',
+            4 => 'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba',
+            5 => 'https://images.unsplash.com/photo-1573865526739-10659fec78a5',
+            6 => 'https://images.unsplash.com/photo-1535268647977-a403b69fc756',
+            7 => 'https://images.unsplash.com/photo-1581888227599-779811939961',
+            8 => 'https://images.unsplash.com/photo-1444212477490-ca407925329e',
+            9 => 'https://images.unsplash.com/photo-1596492784531-6e6eb5ea9993',
+            10 => 'https://images.unsplash.com/photo-1552053831-71594a27632d',
+            11 => 'https://images.unsplash.com/photo-1533738363-b7f9aef128ce',
+            12 => 'https://images.unsplash.com/photo-1587300003388-59208cc962cb',
+            13 => 'https://images.unsplash.com/photo-1591561954557-26941169b49e',
+            14 => 'https://images.unsplash.com/photo-1548199973-03cce0bbc87b',
+            15 => 'https://images.unsplash.com/photo-1504595403659-9088ce801e29'
+        ];
 
         // 7. POBLAMIENTO MASIVO AUTOMÁTICO (15 Protectoras, 150 Animales, 150 Eventos)
         for ($i = 1; $i <= 15; $i++) {
             
-            // Creamos cada protectora utilizando el factory con su estado dinámico
+            // Creamos cada protectora inyectando secuencialmente el nombre, email y su imagen única
             $nuevaProtectora = User::factory()->protectora()->create([
                 'name' => "Protectora Albergue " . $i,
                 'email' => "protectora" . $i . "@test.com",
+                'logo_url' => $fotosSecuenciales[$i]
             ]);
 
-            // Generamos exactamente 10 animales para ESTA protectora en concreto
+            // Generamos exactamente 10 animales asignados a esta protectora
             Animal::factory()
                 ->count(10)
                 ->create([
                     'user_id' => $nuevaProtectora->id,
-                    'especie_id' => rand($perro->id, $gato->id), // Usa los IDs reales de tus especies guardadas
+                    'especie_id' => rand($perro->id, $gato->id),
                 ]);
 
-            // Generamos exactamente 10 eventos para ESTA protectora en concreto
+            // Generamos exactamente 10 eventos asignados a esta protectora
             Evento::factory()
                 ->count(10)
                 ->create([
